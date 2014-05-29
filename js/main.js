4bb80316c9d0b98baf12735b142a70539bc3c3d7
Botao <wxt2005@gmail.com>
2014-05-29 21:25:37 +0800

    IE 6 display bug fix #2

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ var bangumis = document.getElementById("bangumi_list").getElementsByTagName("tr"
 var selectButton = function(buttonNum) {
     for (var i = 0; i < navButtons.length; i++) {
         if (i === buttonNum) {
-            navButtons[i].setAttribute("class", "selected");
+            navButtons[i].className = "selected";
         } else {
-            navButtons[i].setAttribute("class", "");
+            navButtons[i].className = "";
         }
     }
     colorTable();
@@ -21,19 +21,19 @@ var selectButton = function(buttonNum) {
 var hideOtherBangumi = function(weekDay) {
     if (weekDay === 8) {
         for (var i = 0; i < bangumis.length; i++) {
-            bangumis[i].setAttribute("class", "");
+            bangumis[i].className = "";
         }
     } else {
         for (var i = 0; i < bangumis.length; i++) {
             var bangumiInfos = bangumis[i].getElementsByTagName("td"),
                 timeJp = bangumiInfos[1].innerHTML,
                 timeCn = bangumiInfos[2].innerHTML;
-            if (timeCn.indexOf("N/A") < 0 && timeCn.indexOf(weekDayList[weekDay]) < 0) {
-                bangumis[i].setAttribute("class", "hide");
-            } else if (timeCn.indexOf("N/A") > 0 && timeJp.indexOf(weekDayList[weekDay]) < 0) {
-                bangumis[i].setAttribute("class", "hide");
+            if (timeCn.indexOf("N/A") === -1 && timeCn.indexOf(weekDayList[weekDay]) === -1) {
+                bangumis[i].className = "hide";
+            } else if (timeCn.indexOf("N/A") !== -1 && timeJp.indexOf(weekDayList[weekDay]) === -1) {
+                bangumis[i].className = "hide";
             } else {
-                bangumis[i].setAttribute("class", "");
+                bangumis[i].className = "";
             }
         }
     }
@@ -57,16 +57,27 @@ var bindButton = function() {
 var colorTable = function() {
     var colored = true;
     for (var i = 0; i < bangumis.length; i++) {
-        if (colored === false && bangumis[i].getAttribute("class") !== "hide") {
-            bangumis[i].setAttribute("class", "color");
+        if (colored === false && bangumis[i].className !== "hide") {
+            bangumis[i].className = "color";
             colored = true;
-        } else if (colored === true && bangumis[i].getAttribute("class") !== "hide"){
+        } else if (colored === true && bangumis[i].className !== "hide"){
             colored = false;
         }
     }
 };
 
+//fix IE bug
+var disableIeSpan = function(){
+    if(navigator.appName === "Microsoft Internet Explorer" && +navigator.appVersion.match(/MSIE\s(\d+)/)[1] < 9) {
+        for (var i = 0, l = bangumis.length; i < l; i++) {
+             var span = bangumis[i].getElementsByTagName("span")[0];
+             span.style.cssText = "display:none";
+        }
+    }
+};
+
 window.onload = function(){
+    disableIeSpan();
     hideOtherBangumi(day);
     selectButton((function(i){ return i !== 0 ? j = i - 1 : j = 6})(day)); //week day to button number
     bindButton();
