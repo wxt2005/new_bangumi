00cf09f5232ba866f2facc1df634cd67762f638b
Botao <wxt2005@gmail.com>
2014-06-11 01:12:11 +0800

    add search area and order button

--- js/app.js
+++ js/app.js
@@ -3,8 +3,27 @@
 var app = angular.module('BangumiList', []);
 
 app.controller('ListController', ['$scope', '$http', function($scope, $http) {
+    $scope.reverse = false;
+    $scope.ordered = 'jp';
+
+    //order bangumi list
+    $scope.order = function(items, target, reverse) {
+        var weekDay = 'weekDay' + target.toUpperCase(),
+            time = 'time' + target.toUpperCase();
+        reverse = reverse ? -1 : 1;
+        
+        return items.sort(function(a, b) {
+            if (a[weekDay] === b[weekDay]) {
+                return reverse * (a[time] - b[time]);
+            } else {
+                return reverse * ((a[weekDay] === 0 ? 7 : a[weekDay]) - 
+                                  (b[weekDay] === 0 ? 7 : b[weekDay]));
+            }
+       });
+    };
+
     $http.get('json/bangumi-1404.json').success(function(data) {
-                $scope.bangumis = data;
+                $scope.bangumis = $scope.order(data, 'jp', false);
               });
 }]);
 
@@ -12,6 +31,12 @@ app.filter('weekday', function() {
     var weekDayList = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
     return function(weekDayNum) {
         return weekDayList[weekDayNum];
+    };
+});
+
+app.filter('time', function() {
+    return function(originTime) {
+        return originTime ? originTime.slice(0,2) + ':' + originTime.slice(2) : '';
     };
 });
 
