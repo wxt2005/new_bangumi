688940c64f8e066c34764c18e51b2787eeb89727
Botao <wxt2005@gmail.com>
2014-06-15 10:57:01 +0800

    fix iefix.js

--- js/iefix.js
+++ js/iefix.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //if Array.prototype.indexOf didn't exist, then add one
 if (typeof Array.prototype.indexOf !== 'function') {
     Array.prototype.indexOf = function(target) {
@@ -13,51 +15,46 @@ if (typeof Array.prototype.indexOf !== 'function') {
         }
     };
 }
-
-(function() {
-    'use strict';
-
-    var ieFix = angular.module('ieFix', []);
-    //fix IE7- bugs
+angular.module('ieFix', [])
+    
+//fix IE7- bugs
+.config(['$sceProvider', function($sceProvider) {
     if (navigator.appName === "Microsoft Internet Explorer" && 
-        +navigator.appVersion.match(/MSIE\s(\d+)/)[1] < 8) {
-
-        //fix IE7- $sce:iequirks error
-        ieFix.config(function($sceProvider) {
-            $sceProvider.enabled(false);
-        });
-
-        //fix IE7- ng-class bug
-        ieFix.directive('classFix', function() {
-            return {
-                restrict: 'A',
-                link: function(scope, element, attrs) { 
-                    scope.$watch(function() {
-                        return element[0]['class'];
-                    }, function() {
-                        if (element[0]['class']) {
-                            element[0].className = element[0]['class'];
-                        } else {
-                            element[0].className = '';
-                        }
-                    });
-                }
-            };
-        }); 
+    +navigator.appVersion.match(/MSIE\s(\d+)/)[1] < 8) {
+        $sceProvider.enabled(false);
     }
+}])
 
-    //fix IE8- bugs
+.directive('classFix', function() {
     if (navigator.appName === "Microsoft Internet Explorer" && 
-        +navigator.appVersion.match(/MSIE\s(\d+)/)[1] < 9) {
-
-        //fix IE8- transparent display error
-        ieFix.directive('spanFix', function() {
-            return {
-                restrict: 'A',
-                link: function(scope, element, attrs) { 
-                    element[0].className = 'hide';
-                }
-            };
-        });
+    +navigator.appVersion.match(/MSIE\s(\d+)/)[1] < 8) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) { 
+                scope.$watch(function() {
+                    return element[0]['class'];
+                }, function() {
+                    if (element[0]['class']) {
+                        element[0].className = element[0]['class'];
+                    } else {
+                        element[0].className = '';
+                    }
+                });
+            }
+        };
     }
-})();
+})
+
+.directive('spanFix', function() {
+    if (navigator.appName === "Microsoft Internet Explorer" && 
+    +navigator.appVersion.match(/MSIE\s(\d+)/)[1] < 9) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) { 
+                element[0].className = 'hide';
+            }
+        };
+    }
+});
+
+
