d7e7643b473a74ffde2d989b6d80b60376b40f1c
Botao <wxt2005@gmail.com>
2014-08-11 17:43:02 +0800

    add some comments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//用于获取链接的域名
 function getDomain(url) {
     var re = /^https{0,}:\/\/\w+\.(\w+)\.\w+/i;
     if (url !== '#') {
@@ -14,7 +15,8 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
     var dateNow, weekDayNow, yearNow, monthNow;
     var i = 0, l = 0;
 
-    $scope.navList = [
+    //选择器的tab列表
+    $scope.switcherList = [
         {name: '周一', index: 1, order: 'cn'},
         {name: '周二', index: 2, order: 'cn'},
         {name: '周三', index: 3, order: 'cn'},
@@ -25,20 +27,21 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         {name: '全部', index: -1, order: 'jp'}
     ];
 
+    //支持的站点列表
     $scope.siteList = [
-        {name: 'A站', domain: 'acfun', show: true}, 
-        {name: 'B站', domain: 'bilibili', show: true}, 
-        {name: '搜狐', domain: 'sohu', show: true}, 
-        {name: '优酷', domain: 'youku', show: true}, 
-        {name: '腾讯', domain: 'qq', show: true},
-        {name: '爱奇艺', domain: 'iqiyi', show: true},
-        {name: '乐视', domain: 'letv', show: true},
-        {name: 'PPTV', domain: 'pptv', show: true},
-        {name: '土豆', domain: 'tudou', show: true},
-        {name: '迅雷', domain: 'movie', show: true}
+        {name: 'A站', domain: 'acfun'}, 
+        {name: 'B站', domain: 'bilibili'}, 
+        {name: '搜狐', domain: 'sohu'}, 
+        {name: '优酷', domain: 'youku'}, 
+        {name: '腾讯', domain: 'qq'},
+        {name: '爱奇艺', domain: 'iqiyi'},
+        {name: '乐视', domain: 'letv'},
+        {name: 'PPTV', domain: 'pptv'},
+        {name: '土豆', domain: 'tudou'},
+        {name: '迅雷', domain: 'movie'}
     ];
 
-    //searching query
+    //查询用
     $scope.query = {
         'nextTime': 24,
         'weekDayCN': -1,
@@ -46,7 +49,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         'showNew': false
     };
 
-    //app settings
+    //APP设置
     $scope.setting = {
         'nextTimeMax': 24,   
         'nextTimeMin': 20,
@@ -55,7 +58,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         'newTab': false
     };
 
-    //app status
+    //APP状态
     $scope.status = {
         'nextTime': '',
         'error': false,
@@ -72,7 +75,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         }
     };
 
-    //change link target
+    //切换a标签的target
     $scope.changeTarget = function() {
         if($scope.setting.newTab) {
             $scope.status.linkTarget = '_blank';
@@ -82,7 +85,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         ipCookie('newTab', $scope.setting.newTab, {expires: 365});
     };
 
-    //clear all cookie
+    //清除所有cookies
     $scope.clearCookie = function() {
         var keys = document.cookie.match(/[^ =;]+(?=\=)/g);
         if (keys) {
@@ -102,7 +105,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         }
     };
 
-    //handle link click event
+    //播放地址的控制器
     $scope.linkHandler = function(site, event) {
         if(site === "#") {
             if(event.preventDefault) {
@@ -113,7 +116,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         }
     };
 
-    //change back to init weekDayCN
+    //搜索框清空后恢复原有状态
     $scope.resumeSearch = function() {
         $scope.query.titleCN = '';
         if (!$scope.setting.showAll && !$scope.setting.showAll) {
@@ -121,7 +124,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         }
     };
 
-    //handle search input event
+    //搜索栏的控制器
     $scope.searchHandler = function(event) {
         if ($scope.query.weekDayCN !== -1) {
             $scope.query.weekDayCN = -1;
@@ -131,7 +134,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         }
     };
 
-    //handel switch event
+    //星期选择器的控制器
     $scope.switcherHandler = function(index, order) {
         $scope.query.titleCN = '';
         $scope.query.weekDayCN = index;
@@ -143,7 +146,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         }
     };
 
-    //filter bangumi data
+    //过滤番组数据
     $scope.bgmFilter = function(item) {
         var flag = false;
         if ($scope.query.weekDayCN === -1 ) {
@@ -166,7 +169,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         return flag;
     };
 
-    //to handle hour selecter
+    //小时选择按钮的控制器
     $scope.hourSelectHandler = function(flag) {
         if (flag === '+') {
             $scope.query.nextTime = ($scope.query.nextTime === $scope.setting.nextTimeMax ? $scope.setting.nextTimeMax : $scope.query.nextTime + 1);
@@ -178,7 +181,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         ipCookie('nextTime', $scope.query.nextTime, {expires:365});
     };
 
-    //to check status.nextTime
+    //检查控制器的时间是否超过上下限
     $scope.checkNextTime = function() {
         if ($scope.query.nextTime === $scope.setting.nextTimeMax) {
             $scope.status.nextTime = 'max';
@@ -189,30 +192,32 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         }
     };
 
-    //use cookie to change siteList
-    $scope.getSiteCookie = function(siteList) {
-        return siteList.map(function(site) {
+    //从cookie中获取站点是否显示
+    $scope.getSiteCookie = function() {
+        return $scope.siteList.map(function(site) {
             var value = ipCookie(site.domain);
             if (value !== undefined) {
                 site.show = value;
+            } else {
+                site.show = true;
             }
             return site; 
         });
     };
 
-    //site switch
-    $scope.switchSite = function(domain, show) {
+    //站点开关的控制器
+    $scope.siteHandler = function(domain, show) {
         ipCookie(domain, show, {expires: 365});
         $scope.checkSiteList();
     };
 
-    //setting.showNew switch
-    $scope.switchNewOnly = function() {
+    //只显示新番按钮的控制器
+    $scope.newOnlyHandler = function() {
         ipCookie('showNew', $scope.query.showNew, {expires: 365});
     };
 
-    //showall switch
-    $scope.switchAllOnly = function() {
+    //直接显示全部按钮的控制器
+    $scope.showAllHandler = function() {
         if ($scope.setting.showAll) {
             $scope.query.weekDayCN = -1;
             $scope.order($scope.bangumis, 'jp', false);
@@ -223,14 +228,14 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         ipCookie('showAll', $scope.setting.showAll, {expires: 365});
     };
 
-    //handle archive file button
+    //归档按钮的控制器
     $scope.archiveHandler = function(year, month) {
         $scope.readBgm($scope.getJsonPath(year, month, $scope.archive), 'jp', false);
         $scope.query.weekDayCN = -1;
         $scope.setting.showAll = true;
     };
 
-    //handle status.shadow layer
+    //页面遮罩的控制器
     $scope.status.shadowHandler = function() {
         $scope.status.menu.archive = false;
         $scope.status.menu.display = false;
@@ -238,7 +243,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         $scope.status.shadow = false;
     };
 
-    //handle top bar menu button
+    //顶部菜单按钮的控制器
     $scope.topMenuHandler = function(menuName, flag) {
         $scope.status.menu[menuName] = !$scope.status.menu[menuName];
         if(!flag) {
@@ -246,19 +251,19 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         }
     };
 
-    //check if all bangumi's show is true
+    //检查是否所有站点都被勾选，判断是否显示全选按钮
     $scope.checkSiteList = function() {
+        $scope.status.selectAll = false;
         for(i = 0, l = $scope.siteList.length; i < l; i++) {
             if($scope.siteList[i].show === false) {
                 $scope.status.selectAll = true;
                 break;
             }
-            $scope.status.selectAll = false;
         }
     };
 
-    //for select all button
-    $scope.selectAll = function(flag) {
+    //全选按钮的控制器
+    $scope.selectAllHandler = function(flag) {
         for(i = 0, l = $scope.siteList.length; i < l; i++) {
             $scope.siteList[i].show = flag; 
             ipCookie($scope.siteList[i].domain,$scope.siteList[i].show,{expires:365});
@@ -266,7 +271,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         $scope.status.selectAll = !$scope.status.selectAll;
     };
 
-    //order bangumi list
+    //排序番组数据
     $scope.order = function(items, target, reverseFlag) {
         var weekDay = 'weekDay' + target.toUpperCase(),
         time = 'time' + target.toUpperCase();
@@ -286,12 +291,11 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
                 }
             } else {
                 return reverseFlag * (a[weekDay] - b[weekDay]);
-                //return reverseFlag * ((a[weekDay] === 0 ? 7 : a[weekDay]) - (b[weekDay] === 0 ? 7 : b[weekDay]));
             }
         });
     };
 
-    //turn current month to bangumi season
+    //将当前月份转换为季度
     $scope.monthToSeason = function(month) {
         switch (true) {
             case (month < 4):
@@ -307,7 +311,7 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         }
     };
 
-    //use year month to get json file path
+    //通过年份和月份得到数据文件的路径
     $scope.getJsonPath = function(year, month, archive) {
         for (var file in archive) {
             if (archive[file].year == year) {
@@ -320,31 +324,27 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
                     }
                 }
                 if ($scope.monthRead === 1) {
-                    //console.log('failed to get json path, try year - 1, 10');
                     return $scope.getJsonPath(year - 1, 10, archive);
                 } else {
-                    //console.log('failed to get json path, try year, month - 3');
                     return $scope.getJsonPath(year, month - 3, archive);
                 }
             }
         }
-        //console.log('failed to get json path, try year - 1');
         return $scope.getJsonPath(year - 1, month, archive);
     };
 
-    //use $http to get bangumi data
+    //用于获取番组数据
     $scope.readBgm = function(filePath, order, reverse) {
-        //ngProgressLite.start();
         $http.get(filePath)
         .success(function(data, status, headers) {
             $scope.bangumis = $scope.order(data, order, reverse);
             $scope.query.titleCN = '';
+            //获取数据文件最后修改时间
             if (headers('Last-Modified')) {
                 var tempDate = new Date(headers('Last-Modified'));
                 $scope.status.lastModifieded = '数据更新日期: ' + tempDate.getFullYear() + '年' + 
                     (tempDate.getMonth() + 1) + '月' + tempDate.getDate() + '日';
             }
-            //ngProgressLite.done();
         })
         .error(function(data, status) {
             $scope.status.error = true;
@@ -352,10 +352,10 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
         });
     };
 
-    //use $http to get archive data, then init page
+    //获得归档内容，并初始化页面
     $http.get('json/archive.json')
     .success(function(data, status, headers) {
-        //if server time didn't exist, use local time
+        //如果无法获取服务器时间，则使用本地时间
         dateNow = headers('Date') ? new Date(headers('Date')) : new Date();
         weekDayNow = dateNow.getDay();
         yearNow = dateNow.getFullYear();
@@ -366,25 +366,31 @@ angular.module('BangumiList', ['ieFix', 'ipCookie'])
             data[file].show = data[file].year == yearNow ? true : false;
         }
         $scope.archive = data;
+
+        $scope.setting.showAll = ipCookie('showAll') || false;
         if ($scope.setting.showAll) {
             $scope.readBgm($scope.getJsonPath(yearNow, monthNow, data), 'jp', false);
             $scope.query.weekDayCN = -1;
+            console.log($scope.query.weekDayCN);
         } else {
             $scope.readBgm($scope.getJsonPath(yearNow, monthNow, data), $scope.status.ordered, !$scope.status.reversed);
         }
+        
+        $scope.siteList = $scope.getSiteCookie();
+        $scope.checkSiteList();
+
+        $scope.query.showNew = ipCookie('showNew') || false;
+
+        $scope.query.nextTime = ipCookie('nextTime') || 24;
+        $scope.checkNextTime();
+
+        $scope.setting.newTab = ipCookie('newTab') || false;
+        $scope.changeTarget();
     })
     .error(function(data, status) {
         $scope.status.error = true;
         $scope.status.errorMsg = '读取 archive.json 出错. 错误代码: ' + status + '. 请点击上方的“提交问题”按钮.';
     });
-    $scope.siteList = $scope.getSiteCookie($scope.siteList);
-    $scope.query.showNew = ipCookie('showNew') || false;
-    $scope.query.nextTime = ipCookie('nextTime') || 24;
-    $scope.setting.showAll = ipCookie('showAll') || false;
-    $scope.setting.newTab = ipCookie('newTab') || false;
-    $scope.changeTarget();
-    $scope.checkSiteList();
-    $scope.checkNextTime();
 }])
 
 /*
@@ -424,14 +430,14 @@ return false;
 })*/
 
 
-//filter used to format time data
+//显示全选还是全不选
 .filter('selectAllButton', function() {
     return function(flag) {
         return flag ? '全选' : '全不选';
     };
 })
 
-//filter used to format weekday data
+//格式化星期名称
 .filter('weekday', function() {
     var weekDayList = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
     return function(weekDayNum) {
@@ -439,14 +445,14 @@ return false;
     };
 })
 
-//filter used to format time data
+//格式化播放时间
 .filter('time', function() {
     return function(originTime) {
         return originTime ? originTime.slice(0,2) + ':' + originTime.slice(2) : '(预计)';
     };
 })
 
-//filter used to format month data
+//将数字月份转化为中文月份
 .filter('monthCN', function() {
     return function(originMonth) {
         switch (+originMonth) {
@@ -464,7 +470,7 @@ return false;
     };
 })
 
-//filter used to format bangumi link
+//将站点网址转化为站点名称
 .filter('onair', function() {
     return function(link) {
         switch (getDomain(link)) {
@@ -496,7 +502,7 @@ return false;
     };
 })
 
-//filter use to choose and order onair site
+//过滤并排列站点列表
 .filter('linkFilter', function() {
     return function(array, siteList) {
         var nArray = [];
