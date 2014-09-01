fc5b481daec09a1d81b0a3e2ffadac5a5300f2b6
Botao <wxt2005@gmail.com>
2014-09-01 11:22:29 +0800

    fix link order

--- js/main.js
+++ js/main.js
@@ -239,12 +239,17 @@ $(function() {
     function sortLink(a, b) {
         a = formatLink(getDomain(a));
         b = formatLink(getDomain(b));
-        if (a < b) {
-            return -1;
-        } else if (a > b) {
-            return 1;
+        // 将文字长的链接排后面
+        if (a.length === b.length) {
+            if (a < b) {
+                return -1;
+            } else if (a > b) {
+                return 1;
+            } else {
+                return 0;
+            }
         } else {
-            return 0;
+            return a.length - b.length;
         }
     }
 
