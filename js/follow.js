1f620891a757c9df5615376471c6e92641874328
xzk <xiaozhikang0916@gmail.com>
2015-04-18 21:33:56 +0800

    Add 'addFollow' function

--- js/follow.js
+++ js/follow.js
@@ -1,5 +1,21 @@
 /* 
+ * 关注功能的具体JS实现
+ * 使用LocalStorage将关注信息保存在用户本地
+ * 
  */
 
-
-
+/*
+ * 
+ */
+function addFollow( bgmName ) {
+    
+    var list = localStorage.getItem('bangumi');
+    if (list !== null) {
+        list = list.split(',');
+    } else {
+        list = new Array();
+    }
+    list.push(bgmName);
+    localStorage.setItem(list);
+    
+}
